8e3794e448e11d99ce73653dea1fc7b321f2b7d5
Hiroshi Ueki <ueki@sonicgarden.jp>
2022-02-18 13:29:37 +0900

    ロジック部分

---
+++ src/chrono.ts
@@ -0,0 +1,9 @@
+type ChronoConfig = {
+  startTime: Date;
+  endTime: Date;
+  secondsToGenerate: number;
+};
+
+export const chrono = (currentTime: Date, { startTime, endTime, secondsToGenerate }: ChronoConfig) => {
+  return;
+};
