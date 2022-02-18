30bc367b5b7260f41e73e551795625110a8b96b4
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-19 00:20:38 +0900

    fix

--- src/utils/chrono.test.ts
+++ src/utils/chrono.test.ts
@@ -1,1 +1,11 @@
+import { isSameDay } from "date-fns";
+import { expect, it } from "vitest";
 import { chrono } from "./chrono";
+
+it("", () => {
+  const date1 = new Date(2000, 0, 1, 0, 0);
+  const date2 = new Date(2000, 0, 1, 12, 0);
+  console.log(date1);
+  console.log(date2);
+  expect(isSameDay(date1, date2)).toBe(true);
+});
