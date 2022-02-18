34beb4edbed321f04a922881475e979c77c8cc35
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-18 14:50:49 +0900

    add chrono

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { Center } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { AppClock } from "./AppClock";
 import { AppLayout } from "./AppLayout";
+import { chrono } from "./chrono";
 
 function App() {
+  useEffect(() => {
+    const currentDate = new Date(2022, 2 - 1, 18, 23, 59);
+
+    const startDate = new Date(2022, 2 - 1, 18, 9, 0);
+    const endDate = new Date(2022, 2 - 1, 18, 18, 0);
+
+    const hoursToGenerate = 1;
+
+    const chronoDate = chrono(currentDate, { startDate, endDate, hoursToGenerate });
+
+    console.log(chronoDate);
+  }, []);
   return (
     <AppLayout>
       <AppClock />
--- src/chrono.ts
+++ src/chrono.ts
@@ -1,9 +1,37 @@
+import { endOfToday } from "date-fns";
+
+const MS_PER_HOUR = 3_600 * 1_000;
+
 type ChronoConfig = {
-  startTime: Date;
-  endTime: Date;
-  secondsToGenerate: number;
+  startDate: Date;
+  endDate: Date;
+  hoursToGenerate: number;
 };
 
-export const chrono = (currentTime: Date, { startTime, endTime, secondsToGenerate }: ChronoConfig) => {
-  return;
+export const chrono = (
+  currentDate: Date,
+  { startDate, endDate, hoursToGenerate }: ChronoConfig
+) => {
+  const currentTime = currentDate.getTime();
+  const startTime = startDate.getTime();
+  const endTime = endDate.getTime();
+
+  const generateTime = hoursToGenerate * MS_PER_HOUR;
+
+  if (currentTime <= startTime) return new Date();
+
+  const realTimeFromStartToEnd = endTime - startTime;
+  const ratio = realTimeFromStartToEnd / (realTimeFromStartToEnd - generateTime);
+  const realTimeFromStart = currentTime - startTime;
+  const chronoTimeFromStart = realTimeFromStart * ratio;
+
+  if (chronoTimeFromStart <= realTimeFromStartToEnd) {
+    return new Date(startTime + chronoTimeFromStart);
+  } else {
+    const realTimeFromEndToEndOfToday = endOfToday().getTime() - currentTime - generateTime;
+    const chronoTimeFromEndToEndOfToday = endOfToday().getTime() - endTime;
+    const ratio = chronoTimeFromEndToEndOfToday / realTimeFromEndToEndOfToday;
+    const chronoTimeFromEnd = realTimeFromEndToEndOfToday * ratio;
+    return new Date(endTime + chronoTimeFromEnd);
+  }
 };
