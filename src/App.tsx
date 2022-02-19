ea4861be22c62a3b33d7958977f411f0a9a89da8
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-19 12:46:15 +0900

    favicon 消した

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,8 @@ import { Box } from "@chakra-ui/react";
 import { AppClock } from "./components/AppClock";
 import { AppLayout } from "./components/AppLayout";
 import { SettingsProvider } from "./context/settings";
-import useTitle from "react-use/lib/useTitle";
 
 function App() {
-  useTitle("ChronoHacker");
   return (
     <SettingsProvider>
       <AppLayout>
