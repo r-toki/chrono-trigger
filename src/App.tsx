9a413f2617d37930ffa55d36a43fe4485f267bff
Rei-Sogawa <natto.and.eggs@gmail.com>
2022-02-19 12:42:34 +0900

    title タグを設定

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import { Box } from "@chakra-ui/react";
 import { AppClock } from "./components/AppClock";
 import { AppLayout } from "./components/AppLayout";
 import { SettingsProvider } from "./context/settings";
+import useTitle from "react-use/lib/useTitle";
 
 function App() {
+  useTitle("ChronoHacker");
   return (
     <SettingsProvider>
       <AppLayout>
